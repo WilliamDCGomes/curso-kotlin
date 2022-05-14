9e56bc8428215c563e2422d164251ae5e50c0517
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2022-05-14 17:09:47 -0300

    Criado primeiro projeto hello word

---
+++ app/src/main/java/com/example/primeiroprojeto/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.primeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.meuprojeto.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
